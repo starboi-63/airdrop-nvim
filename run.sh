5b10c662e194c5a8930c7d4a2592dfdcb35ded08
Kazuya Erdos <kazuya_erdos@brown.edu>
2025-01-21 09:11:45 -0500

    remove some prereqs and no clangd

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ echo "=== AIRDROP NVIM ==="
 sudo apt-get update && sudo apt-get upgrade -y
 
 echo "=== INSTALLING PREREQS ==="
-sudo apt-get install ninja-build gettext cmake curl build-essential unzip ripgrep golang clangd -y
+sudo apt-get install cmake curl unzip ripgrep libclang-10-dev golang -y
 
 echo "=== INSTALLING NEOVIM ==="
 cd
@@ -14,15 +14,11 @@ sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
 
-# git clone https://github.com/neovim/neovim.git && cd neovim
-# make CMAKE_BUILD_TYPE=Release
-# sudo make install
-
-echo "=== INSTALLING CLANGD ==="
-cp -r ~/airdrop-nvim/clangd/ ~/.local/share/nvim/mason/packages/
-wget -O ~/.local/share/nvim/mason/packages/clangd/clangd_19.1.2.zip https://github.com/clangd/clangd/releases/download/19.1.2/clangd-linux-19.1.2.zip
-unzip ~/.local/share/nvim/mason/packages/clangd/clangd_19.1.2.zip -d ~/.local/share/nvim/mason/packages/clangd/clangd_19.1.2 && rm ~/.local/share/nvim/mason/packages/clangd/clangd_19.1.2.zip
-ln -s /usr/bin/clangd ~/.local/share/nvim/mason/bin/clangd
+# echo "=== INSTALLING CLANGD ==="
+# cp -r ~/airdrop-nvim/clangd/ ~/.local/share/nvim/mason/packages/
+# wget -O ~/.local/share/nvim/mason/packages/clangd/clangd_19.1.2.zip https://github.com/clangd/clangd/releases/download/19.1.2/clangd-linux-19.1.2.zip
+# unzip ~/.local/share/nvim/mason/packages/clangd/clangd_19.1.2.zip -d ~/.local/share/nvim/mason/packages/clangd/clangd_19.1.2 && rm ~/.local/share/nvim/mason/packages/clangd/clangd_19.1.2.zip
+# ln -s /usr/bin/clangd ~/.local/share/nvim/mason/bin/clangd
 
 echo "=== INSTALLING NEOVIM CONFIG ==="
 mkdir ~/.config && cd ~/.config
